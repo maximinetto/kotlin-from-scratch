f54b75b4b88313b9af554da8d0515ec3eaad0570
Maximiliano Minetto <maximinetto@gmail.com>
2023-11-04 14:55:29 -0300

    First examples

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,54 @@
+fun main() {
+    /*val name: String?
+    val age: Short?
+
+    print("Enter a name: ")
+    name = readln()
+    print("Enter a age: ")
+    age = readln().toShort()
+
+    val person = Person(name, age)
+    person.sayHi() */
+
+    /*print("Enter your salary in pesos: ")
+    val salary = readln().toInt()
+
+    println("Your salary is $salary")
+
+    if (salary >= 38000) {
+        println("You have to pay income tax")
+    }else {
+        println("You do not have to pay income tax")
+    }*/
+
+    /*print("Enter a value: ")
+    val value1 = readln().toInt();
+
+    print("Enter an other value: ")
+    val value2 = readln().toInt();
+
+    val greater = if(value1 > value2) {
+        value1
+    }else {
+        value2
+    }
+
+    println("The greatest value is $greater")*/
+
+    /*for (i in 1..10) {
+        println("$i. My name is Maximiliano Minetto")
+    }
+
+    val teams = arrayOf("Real Madrid", "Barcelona", "Manchester City", "Bayern Munich", "Liverpool")
+
+    println("Teams: ")
+    for (team in teams){
+        println(team)
+    }*/
+}
+
+/*class Person(private val name: String, private val age: Short) {
+    fun sayHi(){
+        println("Hello. My name is $name and I am $age years old.")
+    }
+}*/
